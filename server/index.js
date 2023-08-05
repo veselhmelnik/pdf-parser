161f8d2983bfb07855cdb009c2f2289d82074afa
Nick <nikitadomitrak@gmail.com>
2023-08-05 16:14:19 +0300

    connect server with client

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const pdfParse = require("pdf-parse");
+const path = require("path")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.post("/extract-text", (req, res) => {
   if (!req.files && !req.files.pdfFile) {
