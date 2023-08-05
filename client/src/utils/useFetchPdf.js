15235b3b15106fff38b565ed8b6ae867778b3d2b
Nick <nikitadomitrak@gmail.com>
2023-08-05 16:06:37 +0300

    prevSet

--- client/src/utils/useFetchPdf.js
+++ client/src/utils/useFetchPdf.js
@@ -8,19 +8,10 @@ export default function useFetchPdf () {
 
   let headers = new Headers();
 
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-
-  headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-  headers.append('Access-Control-Allow-Credentials', 'true');
-
-  headers.append('GET', 'POST', 'OPTIONS');
-
     const fetchPdf = async (formData) => {
     try {
       setLoading(true);
       await fetch(URL, {
-        headers: headers,
         method: "post",
         body: formData,
       })
